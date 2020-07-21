5e01bcd4ed67c20bcba3922749eb0151d508aa26
pastukhVitalii <pastukh.v.7@gmail.com>
2020-07-21 14:47:20 +0300

    react hooks useDispatch, useSelector

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -1,8 +1,32 @@
 import React, {useEffect} from "react";
-import {connect} from "react-redux";
-import {AppStateType} from "../../redux/store";
 import {getCounter, incCounter} from "../../redux/counterReducer";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStateType} from "../../redux/store";
 
+const Counter = () => {
+
+    const dispatch = useDispatch()
+    const value = useSelector((state: AppStateType) => state.counter.value)
+
+    useEffect(() => {
+        dispatch(getCounter())
+    }, [])
+
+    const increment = () => {
+        dispatch(incCounter())
+    }
+    return (
+        <div>
+            <h1>Counter</h1>
+            <p>value: <strong>{value}</strong></p>
+            <button onClick={increment}>Add</button>
+        </div>
+    )
+}
+
+export default Counter;
+
+/*
 type MapStatePropsType = {
     value: number
 }
@@ -24,7 +48,7 @@ debugger
             <h1>Counter</h1>
             <p>value: <strong>{props.value}</strong></p>
             <button onClick={props.incCounter}>Add</button>
-            {/*<button onClick={() => props.incCounter(props.value)}>Add</button> передаєм значення лічильника в thunk і там його збільшуємо на одиницю */}
+            {/!*<button onClick={() => props.incCounter(props.value)}>Add</button> передаєм значення лічильника в thunk і там його збільшуємо на одиницю *!/}
         </div>
     )
 }
@@ -35,4 +59,4 @@ const mapStateToProps = (state: AppStateType): MapStatePropsType => {
     }
 }
 
-export default connect(mapStateToProps, {getCounter, incCounter})(Counter);
+export default connect(mapStateToProps, {getCounter, incCounter})(Counter);*/
